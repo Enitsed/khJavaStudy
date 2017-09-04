5e5c797ccf30892869f49ba66995512c5946c688
enitsed <sllki1@naver.com>
2017-09-04 13:26:05 +0900

    0904 Home work

--- src/java0831_inheritance/HomeWork0901.java
+++ src/java0831_inheritance/HomeWork0901.java
@@ -1,18 +1,25 @@
 package java0831_inheritance;
 
 /* 상속클래스를 이용하여 문제 만들기
- * 
+ * [출력 결과]
+ * BOQ 순살치킨 입니다.
+ * BOQ 치킨, 소 사이즈입니다.
  */
 
 class Chicken {
-	private int size;
+	private String size;
 	private String name;
 
-	public int getSize() {
+	Chicken(String name, String size) {
+		this.size = size;
+		this.name = name;
+	}
+
+	public String getSize() {
 		return size;
 	}
 
-	public void setSize(int size) {
+	public void setSize(String size) {
 		this.size = size;
 	}
 
@@ -30,16 +37,38 @@ interface Bornless {
 	void sunsal();
 }
 
-class FriedChicken extends Chicken {
+class FriedChicken extends Chicken implements Bornless {
+
+	public FriedChicken(String name, String size) {
+		super(name, size);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void sunsal() {
+		System.out.println(super.getName() + " 순살치킨 입니다.");
+	}
 
 }
 
-class ChickenBarbeque extends Chicken {
+class ChickenBarbeque extends Chicken implements Bornless {
+
+	public ChickenBarbeque(String name, String size) {
+		super(name, size);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void sunsal() {
+		System.out.println(super.getName() + " 순살치킨 입니다.");
+	}
 
 }
 
 public class HomeWork0901 {
 	public static void main(String[] args) {
-
+		FriedChicken fc = new FriedChicken("BOQ", "소");
+		fc.sunsal();
+		System.out.printf("%s 치킨, %s 사이즈입니다.", fc.getName(), fc.getSize());
 	}
 }
---
+++ src/java0831_inheritance/HomeWork0902.java
@@ -0,0 +1,45 @@
+package java0831_inheritance;
+
+/* 상속클래스를 이용하여 문제 만들기
+ * [출력 결과]
+ * BOQ 순살치킨 입니다.
+ * BOQ 치킨, 소 사이즈입니다.
+ */
+
+// 주석을 해제하세요
+/*class Chicken {
+	private String size;
+	private String name;
+
+	Chicken(String name, String size) {
+		this.size = size;
+		this.name = name;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}*/
+
+public class HomeWork0902 {
+	public static void main(String[] args) {
+		// 주석을 해제하세요
+		/*
+		 * FriedChicken fc = new FriedChicken("BOQ", "소"); fc.sunsal();
+		 * System.out.printf("%s 치킨, %s 사이즈입니다.", fc.getName(), fc.getSize());
+		 */
+	}
+}
