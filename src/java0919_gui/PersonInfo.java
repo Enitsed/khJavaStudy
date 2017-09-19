8b1c463fa5cc70c099a6948c65688957f1ef8050
enitsed <sllki1@naver.com>
2017-09-19 21:22:37 +0900

    0919 B.C.

--- src/java0919_gui/PersonInfo.java
+++ src/java0919_gui/PersonInfo.java
@@ -5,7 +5,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -21,6 +27,9 @@ public class PersonInfo extends JFrame implements ActionListener, MouseListener 
 		main = new PersonMain();
 		tool = new PersonToolbar();
 
+		menu = new PersonMenu();
+		setJMenuBar(menu);
+
 		add(BorderLayout.CENTER, main);
 		add(BorderLayout.NORTH, tool);
 
@@ -28,15 +37,31 @@ public class PersonInfo extends JFrame implements ActionListener, MouseListener 
 		main.modifyB.addActionListener(this);
 		main.deleteB.addActionListener(this);
 		main.clearB.addActionListener(this);
+
+		menu.fsave.addActionListener(this);
+		tool.saveB.addActionListener(this);
+		menu.fopen.addActionListener(this);
+		tool.openB.addActionListener(this);
+
 		main.table.addMouseListener(this);
-
-		menu = new PersonMenu();
-
-		setJMenuBar(menu);
+		main.registerB.addMouseListener(this);
+		main.modifyB.addMouseListener(this);
+		main.deleteB.addMouseListener(this);
+		main.clearB.addMouseListener(this);
 
 		setSize(520, 550);
 		setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				String title = "종료";
+				String message = "정말로 종료하시겠습니까?";
+				if (getConfirmMessage(title, message) == JOptionPane.YES_OPTION)
+					System.exit(0);
+			}
+		});
+
 	}
 
 	public static void main(String[] args) {
@@ -48,13 +73,15 @@ public class PersonInfo extends JFrame implements ActionListener, MouseListener 
 		Object obj = e.getSource();
 
 		if (obj == main.registerB) {
-			setRegister();
+			setRegister(); // 등록
 		} else if (obj == main.modifyB) {
-			setModify();
+			setModify(); // 수정
 		} else if (obj == main.deleteB) {
-
+			delete(); // 삭제
 		} else if (obj == main.clearB) {
-
+			init(); // 초기화
+		} else if (obj == menu.fsave || obj == tool.saveB) {
+			fileSave();
 		}
 
 	}
@@ -124,6 +151,48 @@ public class PersonInfo extends JFrame implements ActionListener, MouseListener 
 		setCrow(-1);
 	}
 
+	private void delete() {
+		int row = main.table.getSelectedRow();
+		if (getCrow() < 0 || main.table.getValueAt(row, 0) == null) {
+			setMessage("삭제할 레코드를 확인하세요");
+			return;
+		}
+
+		String message = "선택한 레코드를 삭제하시겠습니가?";
+		String title = "삭제";
+		if (getConfirmMessage(title, message) == JOptionPane.NO_OPTION) {
+			setMessage("취소되었습니다.");
+			return;
+		}
+
+		main.tableModel.removeRow(row);
+		setMessage("삭제되었습니다.");
+
+		if (main.table.getRowCount() < 50) {
+			main.tableModel.setRowCount(50);
+		}
+
+		// 새로운 번호 생성
+		for (int i = 0; i < main.table.getRowCount(); i++) {
+			if (main.table.getValueAt(i, 0) == null) {
+				break;
+			}
+			main.table.setValueAt(Integer.toString(i + 1), i, 0);
+		}
+
+	}
+
+	private void init() {
+		setClear();
+		setTableClear();
+	}
+
+	// 테이블 레코드 초기화 및 생성
+	private void setTableClear() {
+		main.tableModel.setRowCount(0); // 테이블 레코드 삭제
+		main.tableModel.setRowCount(50); // 테이블 레코드 생성
+	}
+
 	private void setMessage(String message) {
 		JOptionPane.showMessageDialog(this, message);
 	}
@@ -139,6 +208,10 @@ public class PersonInfo extends JFrame implements ActionListener, MouseListener 
 		// 커서위치
 		main.nameF.requestFocus();
 
+	}
+
+	private int getConfirmMessage(String title, String message) {
+		return JOptionPane.showConfirmDialog(this, message, title, JOptionPane.YES_NO_OPTION);
 	}
 
 	private void setInputRecord() {
@@ -181,13 +254,31 @@ public class PersonInfo extends JFrame implements ActionListener, MouseListener 
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
+		Object obj = e.getSource();
+		if (obj == main.registerB) {
+			main.registerB.setText("Register");
+		} else if (obj == main.modifyB) {
+			main.modifyB.setText("Modify");
+		} else if (obj == main.deleteB) {
+			main.deleteB.setText("Delete");
+		} else if (obj == main.clearB) {
+			main.clearB.setText("Clear");
+		}
 
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
+		Object obj = e.getSource();
+		if (obj == main.registerB) {
+			main.registerB.setText("등록");
+		} else if (obj == main.modifyB) {
+			main.modifyB.setText("수정");
+		} else if (obj == main.deleteB) {
+			main.deleteB.setText("삭제");
+		} else if (obj == main.clearB) {
+			main.clearB.setText("초기화");
+		}
 
 	}
 
@@ -200,6 +291,41 @@ public class PersonInfo extends JFrame implements ActionListener, MouseListener 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
+
+	}
+
+	private void fileSave() {
+		JFileChooser chooser = new JFileChooser();
+		if (chooser.showSaveDialog(this) == JFileChooser.CANCEL_OPTION) {
+			return;
+		}
+		File file = chooser.getSelectedFile();
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter(file);
+			for (int i = 0; i < main.table.getRowCount(); i++) {
+				if (main.table.getValueAt(i, 0) == null) {
+					break;
+				}
+
+				String num = main.table.getValueAt(i, 0).toString();
+				String name = main.table.getValueAt(i, 1).toString();
+				String sex = main.table.getValueAt(i, 2).toString();
+				String tel = main.table.getValueAt(i, 3).toString();
+				String email = main.table.getValueAt(i, 4).toString();
+				fw.write(num + "/" + name + "/" + sex + "/" + tel + "/" + email + "\r\n");
+			}
+			fw.flush();
+			setMessage("저장되었습니다.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
 	}
 
